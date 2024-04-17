bfaa46d19ec9854150b8952b216259755d953f8d
yashchaturvedi18 <yash.chaturvedi18@gmail.com>
2024-04-16 20:46:19 -0400

    deployment fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import config from './config';
+import './styles.css'; 
+
 export default function App() {
   const [city, setCity] = useState('');
   const [weatherData, setWeatherData] = useState(null);
@@ -8,44 +10,45 @@ export default function App() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      if (!city || city.length == 0) {
+      if (!city || city.length === 0) {
         throw new Error('Please enter a city name');
       }
 
-      const response = await fetch(`${config.apiUrl}weather?city=${city}`);
+      const response = await fetch(`https://excited-tutu.cyclic.app/weather?city=${city}`);
       if (!response.ok) {
         throw new Error('Failed to fetch weather data');
       }
 
       const data = await response.json();
       setWeatherData(data);
-      setError(null); // Clear any previous error
+      setError(null); 
     } catch (error) {
       console.error(error);
-      setError(error.message); // Set error message
-      setWeatherData(null); // Clear weather data on error
+      setError(error.message); 
+      setWeatherData(null); 
     }
   };
 
   return (
-    <div>
-      <h1>Weather App</h1>
+    <div className="container">
+      <h1 className="title">Weather App</h1>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Enter city name"
           value={city}
           onChange={(e) => setCity(e.target.value)}
+          className="input"
         />
-        <button type="submit">Get Weather</button>
+        <button type="submit" className="button">Get Weather</button>
       </form>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
+      {error && <p className="error">{error}</p>}
       {weatherData && (
-        <div>
-          <h2>{weatherData.name}</h2>
-          <p>Temperature: {weatherData.main.temp} °F</p>
-          <p>Description: {weatherData.weather[0].description}</p>
-          <p>Wind Speed: {weatherData.wind.speed} miles/hour</p>
+        <div className="weather-info">
+          <h2 className="weather-info__city">{weatherData.name}</h2>
+          <p className="weather-info__item">Temperature: {weatherData.main.temp} °F</p>
+          <p className="weather-info__item">Description: {weatherData.weather[0].description}</p>
+          <p className="weather-info__item">Wind Speed: {weatherData.wind.speed} miles/hour</p>
         </div>
       )}
     </div>
